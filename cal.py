e9e638be089625ca59543e576883d2734b5e6481
leealswl <alswl54321600@gmail.com>
2025-09-15 14:36:58 +0900

    asdf

--- cal.py
+++ cal.py
@@ -10,11 +10,7 @@ if cal == "곱셈":
     print(f_multiple(A, B))
 elif cal == "나눗셈":
     print(f_divided(A, B))
-<<<<<<< HEAD
 elif cal == "덧셈":
-=======
-elif cal == "덧삼":
->>>>>>> 321f034eee41ed92f29a639bd171b25c325aec97
     print(f_add(A, B))
 else :
     print(f_substract(A, B))
