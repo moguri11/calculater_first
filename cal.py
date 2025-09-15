8ba0188ab47db8aba364c1c13b638c5f0aec8208
moguri <mjy312726@naver.com>
2025-09-15 14:16:03 +0900

    sorry

--- cal.py
+++ cal.py
@@ -4,3 +4,14 @@ from f_add import f_add
 from f_substract import f_substract
 
 A, B = map(int, input().strip())
+cal = input()
+
+if cal == "곱셉":
+    print(f_multiple(A, B))
+elif cal == "나눗셈":
+    print(f_divided(A, B))
+elif cal == "덧셉":
+    print(f_add(A, B))
+else :
+    print(f_substract(A, B))
+
