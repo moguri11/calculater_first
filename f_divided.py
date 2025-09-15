898c9d674b4592835a733683d9f89fca3cb696e0
leealswl <alswl54321600@gmail.com>
2025-09-15 14:11:27 +0900

    print delete

--- f_divided.py
+++ f_divided.py
@@ -1,6 +1,3 @@
 def f_divide(a, b):
     result = a / b
     return result
-
-
-print(f_divide(10,2))
--- f_multiple.py
+++ f_multiple.py
@@ -1,7 +1,3 @@
 def f_multiple(a, b):
     return a * b
     return result
-
-print(f_multiple(10,3))
-
-print(1111)
