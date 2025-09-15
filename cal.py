66ddb5d899fcd4d6693c4102b89bae98c792eea0
moguri <mjy312726@naver.com>
2025-09-15 14:52:45 +0900

    final ver

--- cal.py
+++ cal.py
@@ -3,7 +3,7 @@ from f_divided import f_divided
 from f_add import f_add
 from f_substract import f_substract
 
-A, B = map(int, input().strip())
+A, B = map(int, input().split())
 cal = input()
 
 if cal == "곱셈":
