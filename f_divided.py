ec8f82843be3d897940b9581c28a55faedc0d640
moguri <mjy312726@naver.com>
2025-09-15 14:04:01 +0900

    please allow my opnion

--- f_divided.py
+++
@@ -1,6 +0,0 @@
-def f_divide(a, b):
-    result = a / b
-    return result
-
-
-print(f_divide(10,2))
--- f_multiple.py
+++
@@ -1,5 +0,0 @@
-def f_multiple(a, b):
-    return a * b
-    return result
-
-print(f_multiple(10,3))
